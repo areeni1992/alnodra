a0a498d8e96d5c89ebb42a12ad80c39964dcc59a
khaled mohammed <43090321+areeni1992@users.noreply.github.com>
2019-12-25 21:46:40 +0200

    update alnodra project

---
+++ alnodra/blocks/home/inform.php
@@ -0,0 +1,52 @@
+<section class="inform py-5" id="inform">
+    <div class="container">
+        <h3 class="marke text-right">مركز المعرفة <span></span></h3>
+        <div class="row align-items-center text-right">
+                <div class="col-sm-12 col-md-6 col-lg-3 mb-sm-2">
+                    <div class="item border shadow">
+                        <a href="#">
+                            <div class="img">
+                                <img src="uploads/inform1.jpg" alt="" class="img-fluid">
+                            </div>
+                            <p class="m-0 p-3">                                هذا النص مثال لنص يمكن أن يستبدل في نفس المساحة
+                            </p>
+                        </a>
+
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-3 mb-sm-2">
+                    <div class="item border shadow">
+                        <a href="#">
+                            <div class="img">
+                                <img src="uploads/inform2.jpg" alt="" class="img-fluid">
+                            </div>
+                            <p class="m-0 p-3">                                هذا النص مثال لنص يمكن أن يستبدل في نفس المساحة
+                            </p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <div class="item border shadow">
+                        <a href="#">
+                            <div class="img">
+                                <img src="uploads/inform3.jpg" alt="" class="img-fluid">
+                            </div>
+                            <p class="m-0 p-3">                                هذا النص مثال لنص يمكن أن يستبدل في نفس المساحة
+                            </p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <div class="item border shadow">
+                        <a href="#">
+                            <div class="img">
+                                <img src="uploads/inform4.jpg" alt="" class="img-fluid">
+                            </div>
+                            <p class="m-0 p-3">                                هذا النص مثال لنص يمكن أن يستبدل في نفس المساحة
+                            </p>
+                        </a>
+                    </div>
+                </div>
+        </div>
+    </div>
+</section>
---
+++ alnodra/blocks/layouts/header.php
@@ -0,0 +1,58 @@
+<div class="header">
+    <div class="container">
+        <div class="row">
+            <div class="menu-icon col-2 d-md-none text-right mt-3">
+                <a href="#">
+                    <i class="fas fa-bars fa-2x"></i>
+                </a>
+            </div>
+            <div class="order-1 py-lg-3 col-10 text-center col-md-3 pt-sm-3">
+                <div class="logo figure-img pt-md-2">
+                    <a href="#">
+                        <img src="assets/images/logo.png" alt="" class="img-fluid mt-3 mt-md-0">
+                    </a>
+                </div>
+            </div>
+            <div class="text-center order-2 d-none d-md-block col-md-5">
+                <div class="links">
+                    <ul class="m-0 pr-0 d-flex justify-content-center">
+                        <li class="text-center ml-md-1 ml-lg-4">
+                            <a href="#" data-value="home">الرئيسية</a>
+                        </li>
+                        <li class="text-center ml-md-1 ml-lg-4">
+                            <a href="#" data-value="serv">خدمات</a>
+                        </li>
+                        <li class="text-center ml-md-1 ml-lg-4">
+                            <a href="#" data-value="work">أعمالنا</a>
+                        </li>
+                        <li class="text-center ml-md-1 ml-lg-4">
+                            <a href="#" data-value="inform">من نحن</a>
+                        </li>
+                        <li class="text-center ml-md-1 ml-lg-4">
+                            <a href="#" data-value="contact">اتصل بنا</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-auto order-12 d-none d-md-block col-md-4">
+                <div class="social">
+                    <a href="" class="facebook mr-3">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" class="twitter mr-3">
+                        <i class="fab fa-twitter   "></i>
+                    </a>
+                    <a href="#" class="youtube mr-3">
+                        <i class="fab fa-youtube   "></i>
+                    </a>
+                    <a href="#" class="instagram mr-3">
+                        <i class="fab fa-instagram "></i>
+                    </a>
+                    <a href="#" class="searchicon mr-3">
+                        <i class="fas fa-search    "></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
